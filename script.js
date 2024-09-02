ec9951855776c0268101bb45a88302afabdd4bc4
Uria <dorman010@gmail.com>
2024-09-02 17:48:58 +0100

    testing and debugging

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 import { HashMap } from "./hashMap.js";
 const myHashMap = new HashMap();
 myHashMap.set(`Uria`, 35);
-console.log(myHashMap);
+
 myHashMap.set(`Irina`, 32);
-console.log(myHashMap);
+
 myHashMap.set(`asdfgqwerty`, 32);
-console.log(myHashMap);
+
 myHashMap.set(`Uria`, 35);
-console.log(myHashMap);
+
 myHashMap.set(`Irina`, 32);
-console.log(myHashMap.memory);
+
+myHashMap.set(`Uria`, "something new");
+
+myHashMap.set("Ayelet", "another thing");
+
+const test = new HashMap();
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+console.log(test.memory);
+console.log(test.length, test.memory.length);
+test.set("kite", "pink");
+test.set("lion", "golden");
+console.log(test.memory);
+console.log(test.length, test.memory.length);
